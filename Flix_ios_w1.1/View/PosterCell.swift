b1897368997bfb9cd46036245f3e8bdec37b6e60
jinanh <jhuang27@mail.sfsu.edu>
2018-09-10 01:42:45 -0700

    finish 2.0 requirements

---
+++ Flix_ios_w1.1/View/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_ios_w1.1
+//
+//  Created by Jinan Huang on 9/10/18.
+//  Copyright © 2018 Jinan Huang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
